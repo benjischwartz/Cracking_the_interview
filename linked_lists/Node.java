cee83b7d3258acfeef5c1675fc13a82df7e3ab3b
Benji Schwartz <benji.schwartz2013@gmail.com>
2021-11-01 19:50:57 +1100

    completed deleteMiddle

--- linked_lists/Node.java
+++ linked_lists/Node.java
@@ -22,4 +22,12 @@ public class Node {
     public Node getNext() {
         return this.next;
     }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
