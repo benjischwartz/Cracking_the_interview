7196d8c4636043371f03c5d8c337b957e27a5205
Benji Schwartz <benji.schwartz2013@gmail.com>
2021-11-20 19:51:21 +1100

    completed longest palindromic substring with hint

--- leetcode/LongestPalindromicSubString.java
+++ leetcode/LongestPalindromicSubString.java
@@ -1,61 +1,29 @@
 class LongestPalindromicSubString {
     public String longestPalindrome(String s) {
         // For every char in s, calculate the longest palindrome
-        // which has that letter in the center. 
+        // which has that letter in the center.
+        // OR has center in between two letters
         // Keep track of a 'max' variable, and if this is exceeded, 
         // the 'max' palindrome is updated, and so is 'max'.
-        
-        int maxLength = 0;
-        String maxPalindrome = new String();
-        
-        for (int i = 0; i < s.length(); i++) {
-            
-            int upper = i;
-            int lower = i;
-            StringBuilder palindrome = new StringBuilder();
-            
-            while (upper < s.length() && lower >= 0) {
-                if (s.charAt(upper) == s.charAt(lower)) {
-                    
-                    // initial case (will only happen once)
-                    if (upper == lower) {
-                        palindrome.append(s.charAt(lower));
-                    } 
-                    
-                    // we can extend the palindrome
-                    else {
-                        palindrome.insert(0, s.charAt(lower));
-                        palindrome.append(s.charAt(upper));
-                    }       
-                }
-                
-                // walk out the upper and lower
-                upper++;
-                lower--;
+        if (s == null || s.length() < 1) return "";        
+        int start = 0, end = 0;        
+        for (int i = 0; i < s.length(); i++) {            
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1)/2;
+                end = i + len/2;
             }
-            
-            // If substring all one letter, check corner cases
-            
-            // Check upper:
-            if (upper < s.length()) {
-                Boolean same = true;
-                for (int j = 1; j < palindrome.length(); j++) {
-                    if (palindrome.charAt(j) != palindrome.charAt(j - 1)) {
-                        same = false;
-                    }
-                }
-                if (same && palindrome.charAt(upper) == palindrome.charAt(upper - 1)) {
-                    palindrome.append(palindrome.charAt(upper));
-                }
-            }
-            
-            // Check for a new maximum
-            String palString = palindrome.toString();
-            if (palString.length() > maxLength) {
-                maxPalindrome = palString;
-                maxLength = palString.length();
-            }
+        }        
+        return s.substring(start, end + 1);
+    }
+    
+    int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return maxPalindrome;
+        return right - left - 1;
     }
 }
